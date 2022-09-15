faaf5955545bd53eaeeacc89a73f6945eafad125
Wanghx <wanghx0991@outlook.com>
2022-09-15 10:01:05 +0800

    save

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -1,5 +1,6 @@
 from datasource.akshare_source import Akshare
-from metrics import vwap, bbands
+from .metrics import vwap,bbands
+# from metrics import vwap,bbands
 import mplfinance as mpf
 import matplotlib.animation as animation
 
@@ -19,73 +20,55 @@ class Filter:
             self.sig = None
 
 
-if __name__ == "__main__":
-    symbol = "600036"
-    interval = "daily"
-    start_date, end_date = "20220415", "20220908"
-    title = "%s_%s_%s" % (symbol, interval, start_date)
+# if __name__ == "__main__":
+symbol = "600036"
+interval = "daily"
+start_date, end_date = "20220715", "20220908"
+title = "%s_%s_%s" % (symbol, interval, start_date)
 
-    cli = Akshare()
-    data = cli.stock_zh_a_hist(symbol, start_date=start_date, end_date=end_date)
+cli = Akshare()
+data = cli.stock_zh_a_hist(symbol, start_date=start_date, end_date=end_date)
 
-    vwap(data)
-    bbands(data)
+vwap(data)
+bbands(data)
 
-    bbu_col = [col for col in data.columns if "BBU" in col][0]
-    bbl_col = [col for col in data.columns if "BBL" in col][0]
+bbu_col = [col for col in data.columns if "BBU" in col][0]
+bbl_col = [col for col in data.columns if "BBL" in col][0]
 
-    f = Filter()
-    sig = []
-    for i in range(data.shape[0]):
-        f.update(data.iloc[: (i + 1)])
-        sig.append(f.sig)
+f = Filter()
+sig = []
+for i in range(data.shape[0]):
+    f.update(data.iloc[: (i + 1)])
+    sig.append(f.sig)
 
-    data["sig"] = sig
+data["sig"] = sig
 
-    data["up"] = data["low"] - 0.1
-    data.loc[data["sig"] != "up", "up"] = float("nan")
-    data["down"] = data["high"] - 0.1
-    data.loc[data["sig"] != "down", "down"] = float("nan")
-    pass
+data["up"] = data["low"] - 0.1
+data.loc[data["sig"] != "up", "up"] = float('nan')
+data["down"] = data["high"] + 0.1
+data.loc[data["sig"] != "down", "down"] = float('nan')
 
-    # warmup = 10
-    # kwargs = dict(
-    #     type="candle",
-    #     volume=True,
-    #     style=mpf.make_mpf_style(
-    #         base_mpf_style="nightclouds", marketcolors=mpf.make_marketcolors(up="#ff8500", down="#1b90a7", inherit=True)
-    #     ),
-    #     title=title,
-    # )
-    # fig, axes = mpf.plot(data.iloc[0:warmup], returnfig=True, **kwargs)
-    # ax1, ax2 = axes[0], axes[2]
-    #
-    #
-    # def _animate(ival):
-    #     _data = data.iloc[0:(warmup + ival)]
-    #     _kwargs = dict(
-    #         type="candle",
-    #         style=mpf.make_mpf_style(
-    #             base_mpf_style="nightclouds",
-    #             marketcolors=mpf.make_marketcolors(up="#ff8500", down="#1b90a7", inherit=True)
-    #         ),
-    #         # mav=(5, 10),
-    #         addplot=[
-    #             mpf.make_addplot(_data['vwap'], ax=ax1),
-    #             mpf.make_addplot(_data[bbu_col], ax=ax1),
-    #             mpf.make_addplot(_data[bbl_col], ax=ax1),
-    #             mpf.make_addplot(_data['up'],type='scatter',marker='^',color='yellow',markersize=200, ax=ax1),
-    #             mpf.make_addplot(_data['down'], type='scatter', marker='v', color='yellow', markersize=200, ax=ax1),
-    #         ],
-    #     )
-    #     ax1.clear()
-    #     ax2.clear()
-    #     mpf.plot(_data, ax=ax1, volume=ax2, **_kwargs)
-    #
-    #
-    # animation.FuncAnimation(
-    #     fig,
-    #     _animate,
-    #     interval=100,
-    # )
-    # mpf.show()
+
+warmup = 10
+sty = mpf.make_mpf_style(
+        base_mpf_style="nightclouds", marketcolors=mpf.make_marketcolors(up="#ff8500", down="#1b90a7", inherit=True)
+    )
+kwargs = dict(
+    type="candle",
+    volume=True,
+    style=sty,
+    title=title,
+    addplot=[
+        mpf.make_addplot(data['vwap']),
+        mpf.make_addplot(data[bbu_col]),
+        mpf.make_addplot(data[bbl_col]),
+        mpf.make_addplot(data['up'], type='scatter', marker='^', color='yellow', markersize=200),
+        mpf.make_addplot(data['down'], type='scatter', marker='v', color='yellow', markersize=200),
+    ],
+)
+
+
+fig, axes = mpf.plot(data, returnfig=True, **kwargs)
+ax1 = axes[0]
+ax2 = axes[2]
+mpf.show()
